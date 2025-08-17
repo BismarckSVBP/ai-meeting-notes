35e7457e9dbdcca26be1cc4f1c9dd4b45ce4e5f4
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-08-17 18:38:43 +0530

    Delete server directory

--- server/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectDB(uri) {
-  if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(uri, { dbName: "ai_notes" });
-  console.log("MongoDB connected ✅");
-}
