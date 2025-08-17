f77565b254034983ff54bc34e5de9f0b226a5cd7
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-08-17 18:14:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 
+// import dotenv from "dotenv";
+// import { connectDB } from "./config/db.js";
+// import app from "./app.js";
+
+// dotenv.config();
+
+// const PORT = process.env.PORT || 8080;
+
+// const start = async () => {
+//   await connectDB(process.env.MONGODB_URI);
+//   app.listen(PORT, () => console.log(`✅ API listening on ${PORT}`));
+// };
+
+// start();
+
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
 
-const start = async () => {
-  await connectDB(process.env.MONGODB_URI);
-  app.listen(PORT, () => console.log(`✅ API listening on ${PORT}`));
-};
+await connectDB(process.env.MONGODB_URI);
 
-start();
+
+export default app;
